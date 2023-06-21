22c4fc3d9830b2ac1958393c950c2b920b755d0a
naveen p <naveenpendekatla0407@gmail.com>
2023-06-21 12:25:37 +0530

    dispatch

---
+++ dispatch.sh
@@ -0,0 +1,15 @@
+yum install golang -y
+useradd roboshop
+rm -rf /app
+mkdir /app
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
+cd /app
+unzip /tmp/dispatch.zip
+cd /app
+go mod init dispatch
+go get
+go build
+cp /home/centos/shell-practice/dispatch.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl restart dispatch
