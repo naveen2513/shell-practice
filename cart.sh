df1cadc381f79fe8f8d014c15df8fb4f260ba4de
naveen p <naveenpendekatla0407@gmail.com>
2023-06-21 11:31:29 +0530

    cart

---
+++ cart.sh
@@ -0,0 +1,14 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf /app
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+cd /app
+unzip /tmp/cart.zip
+cd /app
+npm install
+cp /home/centos/shell-practice/cart.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl enable cart
+systemctl restart cart
