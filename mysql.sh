de3e12b31474004ecf51b3877ef4f15c58029f0e
naveen p <naveenpendekatla0407@gmail.com>
2023-06-21 12:03:50 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,8 @@
 yum module disable mysql -y
 cp /home/centos/shell-practice/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
+systemctl enable mysqld
+systemctl restart mysqld
