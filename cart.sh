b39b46ca23e2a337023044bdb641deae983e2e7c
naveen p <naveenpendekatla0407@gmail.com>
2023-06-21 11:05:27 +0530

    cart

--- cart.sh
+++ cart.sh
@@ -6,7 +6,6 @@ mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
-cd /app
 npm install
 cp /home/centos/shell-practice/cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
