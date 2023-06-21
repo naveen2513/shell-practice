3ebdf679a3199c41ddab3ba9e8fec34750f91bcb
naveen p <naveenpendekatla0407@gmail.com>
2023-06-21 09:47:57 +0530

    mongodb

---
+++ mongo.sh
@@ -0,0 +1,6 @@
+cp /home/cents/shell-practice/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+sed -i -e "s|127.0.0.1|0.0.0.0|g" /etc/mongod.conf
+systemctl enable mongod
+
+systemctl restart mongod
