d391f64460bfbc9e499f185660f6e69ec9e473dd
naveen p <naveenpendekatla0407@gmail.com>
2023-06-27 13:57:02 +0530

    terraform

--- frontend.sh
+++ frontend.sh
@@ -2,33 +2,33 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-echo -e "\e[32m>>>>>>>>> install nginx<<<<<<<\e[0m"
+print_head "install nginx "
 yum install nginx -y
     fun_status_check $?
 
-echo -e "\e[32m>>>>>>>>> remove lod app content<<<<<<<\e[0m"
+print_head "remove lod app content"
 
 rm -rf /usr/share/nginx/html/*
     fun_status_check $?
 
-echo -e "\e[32m>>>>>>>>> download app cotnent<<<<<<<\e[0m"
+print_head "download app cotnent"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
     fun_status_check $?
 
 
 cd /usr/share/nginx/html  &>>$log_file
-echo -e "\e[32m>>>>>>>>> extract app content<<<<<<<\e[0m"
+print_head "extract app content"
 
 unzip /tmp/frontend.zip &>>$log_file
     fun_status_check $?
 
-echo -e "\e[32m>>>>>>>>> copy roboshop conf file<<<<<<<\e[0m"
+print_head "copy roboshop conf file"
 
 cp /home/centos/shell-practice/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
     fun_status_check $?
 
-echo -e "\e[32m>>>>>>>>> restart nginx<<<<<<<\e[0m"
+print_head "restart nginx"
 
 
 systemctl enable nginx &>>$log_file
